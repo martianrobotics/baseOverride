e3401e6c83eb18f81060b72bca419b3af1a28bbe
martianrobotics <jupiter.arias@gmail.com>
2017-07-05 14:24:34 -0400

    Add project files.

---
+++ baseOverride/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseOverride
+{
+    class xBase
+    {
+        public virtual void Execute()
+        {
+            Console.WriteLine("base executing...");
+        }
+    }
+
+    class xDerived : xBase
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("before executing");
+            base.Execute();
+            Console.WriteLine("after executing");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            xDerived test = new xDerived();
+            test.Execute();
+            Console.ReadKey();
+        }
+    }
+}
